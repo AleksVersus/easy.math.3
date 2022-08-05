d6d00d46fe3e2769227a9dd53b073a727fe365a6
aleksversus <aleksversus@mail.ru>
2022-08-05 19:51:25 +0500

    разделение страниц

---
+++ [disdocs]/[convert_script]/convert_for_site.py
@@ -0,0 +1,14 @@
+
+link_dict={
+	"../manual.html":"http://aleksversus.narod.ru/index/easy_math_3/0-73",
+	"vspomogatelnye_funkcii/0-47.html":"http://aleksversus.narod.ru/index/vspomogatelnye_funkcii/0-74",
+	"operacii_s_drobnymi_chislami/0-14.html":"http://aleksversus.narod.ru/index/operacii_s_drobnymi_chislami/0-75",
+	"logicheskie_operacii/0-58.html":"http://aleksversus.narod.ru/index/logicheskie_operacii/0-76",
+	"operacii_nad_tekstom/0-13.html":"http://aleksversus.narod.ru/index/operacii_s_tekstom/0-77",
+	"operacii_s_shestnadcaterichnymi_chislami/0-15.html":"",
+	"operacii_nad_massivami/0-16.html":"",
+	"operacii_s_celymi_chislami/0-57.html":"",
+	"operacii_s_shestnadcaterichnymi_chislami/0-15":"",
+	"operacii_s_shestnadcaterichnymi_chislami/0-15":"",
+	"operacii_s_shestnadcaterichnymi_chislami/0-15":"",
+}
